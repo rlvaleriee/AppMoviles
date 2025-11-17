ca6659478bfd2419644b8296f0e610576aba72fd
rlvaleriee <163598442+rlvaleriee@users.noreply.github.com>
2025-11-17 13:34:23 -0600

    Mask Firebase config values for security
    Replaced sensitive Firebase configuration values with placeholders for security.

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -9,12 +9,12 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAm7rudkR1PSwHNUav9G7gW8wtuVXnJNzo',
-  authDomain: 'app-citas-2c83a.firebaseapp.com',
-  projectId: 'app-citas-2c83a',
-  storageBucket: 'app-citas-2c83a.appspot.com',
-  messagingSenderId: '176981257185',
-  appId: '1:176981257185:web:2a54bcbf33c669c08e40a0',
+  apiKey: '*******************',
+  authDomain: '*************',
+  projectId: '***********',
+  storageBucket: '***********',
+  messagingSenderId: '************',
+  appId: '****************',
 };
 
 const app = initializeApp(firebaseConfig);
